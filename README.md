475fd3892c2ef2b7c574b23521334a1e521cf719
ex72049969p <oiglesias@cic.es>
2019-02-13 12:04:34 +0100

    Desarrollando la Interface Web

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Desarrollo aplicación Spring MVC v5 + Spring Data JPA
 ## Capítulo 2. Desarrollando y Configurando la Vista y el Controlador
 
 ## Capítulo 3. Desarrollando la Lógica de Negocio
+
+## Capítulo 4. Desarrollando la Interface Web
+Ésta es la Parte 4 del tutorial paso a paso para desarrollar una aplicación web desde cero usando Spring Framework. En la Parte 1 hemos configurado el entorno y montado la aplicación básica. En la Parte 2 hemos mejorado la aplicación que habíamos construido hasta entonces. La Parte 3 añade toda la lógica de negocio y los tests unitarios. Ahora es el momento de construir la interface web para la aplicación.
--- src/main/java/com/prototype/web/SpringappWebConfig.java
+++ src/main/java/com/prototype/web/SpringappWebConfig.java
@@ -3,6 +3,7 @@ package com.prototype.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -12,6 +13,13 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 public class SpringappWebConfig {
 	
+	 @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+	 
 	@Bean
     public ViewResolver internalResourceViewResolver() {
         InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
